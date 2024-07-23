995c540fd1e8e9add1de812fb94fbe516d0e9454
Arakaki68 <rfarakakijr@gmail.com>
2024-07-23 16:51:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const app = express();
 const port = 3000;
-const googleApiKey = 'YOUR_GOOGLE_API_KEY'; // Substitua pela sua chave de API do Google
+const googleApiKey = 'AIzaSyDAiHesZHpQboihaRJbm7vOAN3NNd823DU'; // Chave de API do Google
 
 app.use(bodyParser.json());
 
@@ -14,9 +14,15 @@ app.post('/receber_localizacao', async (req, res) => {
 
     try {
         const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleApiKey}`);
-        const address = response.data.results[0].formatted_address;
-        console.log(`Endereço aproximado: ${address}`);
-        res.send(`Localização recebida com sucesso! Endereço: ${address}`);
+        console.log('Resposta da API de Geocodificação:', response.data);
+        if (response.data.status === "OK" && response.data.results.length > 0) {
+            const address = response.data.results[0].formatted_address;
+            console.log(`Endereço aproximado: ${address}`);
+            res.send(`Localização recebida com sucesso! Endereço: ${address}`);
+        } else {
+            console.error('Nenhum resultado encontrado ou status não OK');
+            res.status(500).send('Erro ao obter o endereço: Nenhum resultado encontrado ou status não OK');
+        }
     } catch (error) {
         console.error('Erro ao obter o endereço:', error);
         res.status(500).send('Erro ao obter o endereço');
